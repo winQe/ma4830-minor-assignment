f8d15706b4eed129d2966db84d2301fc0db8bb91
winQe <85143700+winQe@users.noreply.github.com>
2021-10-03 02:00:32 +0800

    more comments

--- object_properties.c
+++ object_properties.c
@@ -8,7 +8,7 @@ void font_red () { // function to change fontcolor to red
   printf("\033[1;31m");
 }
 
-void font_blue() { // function to change fontcolor to yellow
+void font_blue() { // function to change fontcolor to blue
   printf("\033[0;34m");
 }
 
@@ -20,7 +20,7 @@ int getProperties(int n, char properties[][20], float *result){ //universal func
     float temp = -1;
     char dummy[1000];
     for(int i=0; i<n; i++){  
-        while(1){    
+        while(1){ //type error handling  
             font_blue(); // change font color to blue
             printf("Input %s> ", properties[i]);
             font_reset();    
@@ -166,54 +166,55 @@ int main()
             case 1:; //Rectangle properties
                 char properties1[2][20] = {"Length","Height"}; 
                 float result1[] = {0};
-                getProperties(2,properties1, result1);
-                printf("Perimeter : %.2f\n",2*result1[0]+2*result1[1]);
-                printf("Area : %.2f",result1[0]*result1[1]);
+                getProperties(2,properties1, result1); //prompt user for properties specified in the array
+                printf("Perimeter : %.2f\n",2*result1[0]+2*result1[1]); //perimeter = 2*(length+height)
+                printf("Area : %.2f",result1[0]*result1[1]); //area = length*height
                 break;
             case 2:; //Square properties
                 char properties2[1][20] = {"Side Length"}; 
                 float result2[] = {0};
                 getProperties(1,properties2, result2);
-                printf("Perimeter : %.2f\n",4*result2[0]);
-                printf("Area : %.2f",pow(result2[0],2));
+                printf("Perimeter : %.2f\n",4*result2[0]); //perimeter = 4*length
+                printf("Area : %.2f",pow(result2[0],2)); // area = length^2
                 break;
             case 3:; //Circle properties
                 char properties3[1][20] = {"Radius"}; 
                 float result3[] = {0};
                 getProperties(1,properties3, result3);
-                printf("Perimeter : %.2f\n",2*M_PI*result3[0]);
-                printf("Area : %.2f",M_PI*pow(result3[0],2));
+                printf("Perimeter : %.2f\n",2*M_PI*result3[0]); //perimeter = 2*pi*r
+                printf("Area : %.2f",M_PI*pow(result3[0],2)); // area = pi*r^2
                 break;
             case 4:; //RightTriangle properties
                 char properties4[2][20] = {"Base Length","Height"}; 
                 float result4[] = {0};
                 getProperties(2,properties4, result4);
                 float hypotenuse = sqrt(pow(result4[0],2)+pow(result4[1],2));
-                printf("Hypotenuse : %.2f\n",hypotenuse);
-                printf("Perimeter : %.2f\n",result4[0]+result4[1]+hypotenuse);
-                printf("Area : %.2f",result4[0]*result4[1]/2);
+                printf("Hypotenuse : %.2f\n",hypotenuse); //hypotenuse = sqrt(length^2 + height^2)
+                printf("Perimeter : %.2f\n",result4[0]+result4[1]+hypotenuse); // perimeter = base length+ height + hypotenuse
+                printf("Area : %.2f",result4[0]*result4[1]/2); // area = base length * height / 2
                 break;
             case 101:;//Cube properties
                 char properties101[1][20] = {"Side Length"};
                 float result101[] = {0};
                 getProperties(1, properties101, result101);
-                printf("Base Area: %.2f\n", pow(result101[0],2));
-                printf("Surface Area: %.2f\n", 6*pow(result101[0],2));
-                printf("Volume: %.2f\n", pow(result101[0], 3));
+                printf("Base Area: %.2f\n", pow(result101[0],2)); // base area = length^2
+                printf("Surface Area: %.2f\n", 6*pow(result101[0],2)); // surface area = 6 * base area
+                printf("Volume: %.2f\n", pow(result101[0], 3));// volume = length^3
                 break;
             case 102:; //Blocks/cuboid properties
                 char properties102[3][20] = {"Length","Width","Height"};
                 float result102[] = {0};
                 getProperties(3, properties102, result102);
+                //surface area = 2* length * width + 2 * length * height + 2 * width * height
                 printf("Surface Area: %.2f\n", 2*result102[0]*result102[1]+2*result102[0]*result102[2]+2*result102[1]*result102[2]);
-                printf("Volume: %.2f\n", result102[0]*result102[1]*result102[2]);
+                printf("Volume: %.2f\n", result102[0]*result102[1]*result102[2]); //volume = length * height * width
                 break;
             case 103:; //Sphere properties
                 char properties103[1][20] = {"Radius"};
                 float result103[] = {0};
                 getProperties(1, properties103, result103);
-                printf("Surface Area: %.2f\n", 4*M_PI*pow(result103[0],2));
-                printf("Volume: %.2f\n", 4*M_PI*pow(result103[0],3)/3);
+                printf("Surface Area: %.2f\n", 4*M_PI*pow(result103[0],2)); //surface area = 4*pi*r^2
+                printf("Volume: %.2f\n", 4*M_PI*pow(result103[0],3)/3); // volume = 4/3* pi * r^3
                 break;
             case 104:; //Cylinder
                 char properties104[2][20] = {"Radius","Height"};
@@ -221,10 +222,10 @@ int main()
                 getProperties(2, properties104, result104);
                 float base_area4 = M_PI*pow(result104[0],2);
                 float lateral_area4 = 2*M_PI*result104[0]*result104[1];
-                printf("Base Area: %.2f\n", base_area4);
-                printf("Lateral Surface Area: %.2f\n", lateral_area4);
-                printf("Surface Area: %.2f\n",2*base_area4+lateral_area4);
-                printf("Volume: %.2f\n",base_area4*result104[1]);
+                printf("Base Area: %.2f\n", base_area4); // base area = pi*r^2
+                printf("Lateral Surface Area: %.2f\n", lateral_area4); //lateral area = 2 * pi * radius * height
+                printf("Surface Area: %.2f\n",2*base_area4+lateral_area4); //surface area = 2*base area + lateral area
+                printf("Volume: %.2f\n",base_area4*result104[1]); // volume = base area * height
                 break;        
             case 105:;//Square Pyramid properties
                 char properties105[2][20] = {"Side Length","Height"};
@@ -232,10 +233,10 @@ int main()
                 getProperties(2, properties105, result105);
                 float base_area5 = pow(result105[0],2);
                 float lateral_area5 = result105[0]*sqrt(pow(result105[0],2)+4*pow(result105[1],2));
-                printf("Base Area: %.2f\n", base_area5);
-                printf("Lateral Surface Area: %.2f\n", lateral_area5);
-                printf("Surface Area: %.2f\n",base_area5+lateral_area5);
-                printf("Volume: %.2f\n",base_area5*result105[1]/3);
+                printf("Base Area: %.2f\n", base_area5); //base area = length^2
+                printf("Lateral Surface Area: %.2f\n", lateral_area5);// lateral surface area = length * sqrt(length^2+4*height^2)
+                printf("Surface Area: %.2f\n",base_area5+lateral_area5); // surface area = base area + lateral area
+                printf("Volume: %.2f\n",base_area5*result105[1]/3); //volume = 1/3 * base area * height
                 break;
             case 106:;// Cone properties
                 char properties106[2][20] = {"Radius","Height"};
@@ -244,11 +245,11 @@ int main()
                 float slant=sqrt(pow(result106[0],2)+pow(result106[1],2));
                 float base_area6 = M_PI*pow(result106[0],2);
                 float lateral_area6 =M_PI*result106[0]*slant;
-                printf("Slant Height: %.2f\n",slant);
-                printf("Base Area: %.2f\n",base_area6);
-                printf("Lateral Surface Area: %.2f\n",lateral_area6);
-                printf("Surface Area: %.2f\n", base_area6+lateral_area6);
-                printf("Volume: %.2f\n", base_area6*result106[1]/3);
+                printf("Slant Height: %.2f\n",slant); //slant height = sqrt(radius^2 + height^2)
+                printf("Base Area: %.2f\n",base_area6); // base area = pi*r^2
+                printf("Lateral Surface Area: %.2f\n",lateral_area6); //lateral area = pi * radius * slant
+                printf("Surface Area: %.2f\n", base_area6+lateral_area6); // surface area = base area + lateral area
+                printf("Volume: %.2f\n", base_area6*result106[1]/3); //volume = 1/3 * base area * height
                 break;  
             default:
                 printf("Error, invalid object type!");
